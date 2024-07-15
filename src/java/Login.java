7e19be8e12dac193a1584ccaefd6394a6bcd5409
Aman Dwivedi <amandwivedi71124.com>
2024-07-16 00:37:51 +0530

    Restaurant management system upload

---
+++ src/java/Login.java
@@ -0,0 +1,82 @@
+
+import jakarta.servlet.RequestDispatcher;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Ajeet singh bhadoriy
+ */
+public class Login extends HttpServlet {
+
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            String name = request.getParameter("name");
+           String mobileNumber = request.getParameter("mobileNumber");
+           
+           Class.forName("com.mysql.cj.jdbc.Driver");
+           Connection cn = DriverManager.getConnection("jdbc:mysql://localhost:3306/food","root","Aditi@77470");
+           Statement smt = cn.createStatement();
+           int i = smt.executeUpdate("insert into details (name, mobileNumber) values('"+name+"','"+mobileNumber+"')");
+           
+           if(i>0){
+                RequestDispatcher rd = request.getRequestDispatcher("menu.html");
+                rd.include(request, response);
+           }
+        }
+        catch(Exception e){
+            out.println(e.getMessage());
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
